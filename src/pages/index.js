b16cd6f4aaa7ade4dc5f9729e0b8653ed26b7bcf
baekseojin <seojinbaek100@gmail.com>
2024-06-02 23:42:57 +0900

    ADD: Observation Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export { default as Main } from "./main";
 export { default as Record } from "./record/choice";
-export { default as Observation } from "./record/observation/index";
+export { default as Observation } from "./record/observation";
--- src/pages/record/observation/index.js
+++ src/pages/record/observation/index.js
@@ -1,40 +1,104 @@
 import { styled } from "styled-components";
-
+import { Link } from "react-router-dom";
 import * as C from "../../../components/index";
 import Background from "../../../assets/Background.svg";
+import { ReactComponent as ObservationTimeline } from "../../../assets/ObservationTimeline.svg";
 
 export default function Observation() {
   return (
     <Container>
       <C.Header />
       <ObservationContainer>
-        <Bold>백서진님! 오늘 타인과 대화하며 어떤 일이 있었나요?</Bold>
+        <ObservationTimeline />
+        <Bold>OOO님! 오늘 타인과 대화하며 어떤 일이 있었나요?</Bold>
+        <SemiBold>
+          어떤 상황에서 있는 그대로, 실제로 무엇이 일어나고 있는가를 관찰한다.
+        </SemiBold>
+        <TextBox></TextBox>
+        <Link to="/record/feeling">
+          <Button>다음</Button>
+        </Link>
       </ObservationContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const ObservationContainer = styled.div`
   width: 100vw;
   height: 100vh;
+
   background-color: black;
   background-image: url(${Background});
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
 
-  top: 200px;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  box-sizing: border-box;
+`;
+
+const ObservationContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
   display: flex;
-  justify-content: center;
+  margin-top: 60px;
+
+  align-items: center;
+  flex-direction: column;
 `;
 
 const Bold = styled.div`
   font-size: 36px;
   font-weight: bold;
   color: white;
+  margin-top: 40px;
+`;
+
+const SemiBold = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  color: #7e7e7d;
+  margin-top: 25px;
+`;
+
+const TextBox = styled.textarea`
+  width: 1092px;
+  height: 350px;
+  padding: 10px;
+
+  background-color: white;
+  color: #7e7e7d;
+
+  border-radius: 15px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  resize: none;
+
+  &:focus {
+    border: 2px solid #0a7bff;
+    outline: none;
+  }
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 60px;
+  border-radius: 100px;
+  border: none;
+  margin-bottom: 50px;
+
+  background: linear-gradient(to right, #a799fd, #3423ff);
+
+  color: white;
+  font-weight: Bold;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #6fc5ff;
+    box-shadow: 0 0 20px #6fc5ff50;
+    transform: scale(1.1);
+  }
 `;
