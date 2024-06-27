48ebf4d2ef3edce3597b9c55374a98c54a8a75a3
baekseojin <seojinbaek100@gmail.com>
2024-06-27 22:04:59 +0900

    FEAT(server): records 데이터 DB 서버로 전송

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
 const port = 5000;
@@ -8,17 +9,45 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// 임시 데이터베이스 대신 사용할 변수
-let records = [];
+console.log("MySQL 연결됨~");
 
-// POST 요청을 처리하는 라우트
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "novio",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database.");
+});
+
+// POST 요청 처리
 app.post("/api/record", (req, res) => {
   const record = req.body;
 
+  console.log(record);
+
   if (record) {
-    records.push(record);
-    console.log("Record received:", record);
-    res.status(201).json({ message: "Record received successfully" });
+    const { observation, feeling, need, request } = record;
+    const query =
+      "INSERT INTO record (record_observation, record_feeling, record_need, record_request) VALUES (?, ?, ?, ?)";
+    const values = [observation, feeling, need, request];
+
+    connection.query(query, values, (err, result) => {
+      if (err) {
+        console.error("Error inserting record:", err);
+        res.status(500).json({
+          error: "레코드 데이터를 데이터베이스에 저장하는데 실패했습니다. ",
+        });
+        return;
+      }
+      res.status(201).json({ message: "데이터 성공적으로 저장됨." });
+    });
   } else {
     res.status(400).json({ error: "Invalid data format" });
   }
@@ -26,5 +55,5 @@ app.post("/api/record", (req, res) => {
 
 // 서버 시작
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`서버 여기서 돌아간다 -> http://localhost:${port}`);
 });
